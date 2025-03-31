0db6cac654a4f73a1468c57945cc059719acc07b
Tissinapa <80274382+Tissinapa@users.noreply.github.com>
2025-03-31 10:20:25 +0300

    init test commit

---
+++ src/elevatorproject_mega_master/elevatorproject_mega_master/main.c
@@ -0,0 +1,23 @@
+/*
+ * elevatorproject_mega_master.c
+ *
+ * Created: 31/03/2025 10:16:43
+ * Author : Akseli, Martta, Oskari, Wenhao and Wenjun
+ */ 
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <delay.h>
+#include <stdbool.h>
+#include "lcd.h"
+#include "keypad.h"
+
+
+int main(void)
+{
+    // Testing
+    while (1) 
+    {
+    }
+}
+
