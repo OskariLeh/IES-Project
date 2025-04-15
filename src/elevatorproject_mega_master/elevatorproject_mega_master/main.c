3703027d8765e8b100b6ae8f9deda111c0b3ad63
Tissinapa <80274382+Tissinapa@users.noreply.github.com>
2025-04-15 17:49:09 +0300

    states idle, up, down

--- src/elevatorproject_mega_master/elevatorproject_mega_master/main.c
+++ src/elevatorproject_mega_master/elevatorproject_mega_master/main.c
@@ -22,10 +22,12 @@
 #define IDLE 0
 #define GOINGUP 1
 #define GOINGDOWN 2
-#define DOOROPEN 3
+#define DOOR 3
 #define FAULT 4
 #define EMERGENCY 5
 volatile int8_t state = 0;
+
+
 
 /* Notes
 
@@ -52,9 +54,17 @@ void LCD_setup()
 {
 	lcd_init(LCD_DISP_ON);
 	lcd_clrscr();
-	lcd_puts("Ready?");
+	//lcd_puts("Choose floor");
 	KEYPAD_Init();
 }
+
+bool is_emergency_button_pressed()
+{
+	//
+	return !(PINA & (1 << PA2));		
+}
+
+
 
 int main(void)
 {
@@ -62,44 +72,165 @@ int main(void)
 	LCD_setup();
 	
 	// elevator variables 
-	static int8_t request_floor = 1;
-	static int8_t current_floor = 1;
-	static bool b_doors_open = false;	
+	static int8_t request_floor = 0 , current_floor = 0;
 	
+	static bool b_doors_open = false;	
+	char req_floor_string[4];
+	char curr_floor_string[4];
+	char floor_numb_string[5];
+
+	uint8_t floor_numbers[2] = {0xFF,0xFF};
+	uint8_t index = 0;
+	
+	//emergency button
+	DDRA &= ~(1 << PA2);
+	    
     while (1) 
     {
-		static int8_t request_floor = 1;
-		static int8_t current_floor = 1;
-		static bool b_doors_open = false;
- 		uint8_t key_signal = KEYPAD_GetKey();
-		_delay_ms(300);
+
+ 		
 		
-		// Check if floor button is pressed
-		if(key_signal != 0xFF)
+		
+
+		switch (state)
 		{
-			char key_str[4];
+		case IDLE:
 			
-			if(key_signal >= '1' && key_signal <= '9')
-			{
-				
-				//convert ascii to numeric value
-				uint8_t key_value = key_signal -'0';
-				// numeric to string
-				itoa(key_value, key_str, 10);
-			}
-			else
-			{
-				//Special keys
-				key_str[0] = key_signal;
-				key_str[1] = '\0';
-			}
 			lcd_clrscr();
-			lcd_puts("“Choose the floor:");
+			lcd_puts("Choose floor");
+			lcd_gotoxy(0, 1);
+
+			//uint8_t digits[2] = {0xFF, 0xFF};
+			uint8_t key_signal_1,key_signal_2 ;
+			//char key_signal;
+			//int8_t floor_input  ;
+			////key_signal = KEYPAD_GetKey();
+
+			do {key_signal_1 = KEYPAD_GetKey();} while(!(key_signal_1 >= '0' && key_signal_1 <= '9'));
+			do {key_signal_2 = KEYPAD_GetKey();} while(!(key_signal_2 >= '0' && key_signal_2 <= '9'));
+			
+				
+
+				
+			
+				
+			//if (floor_input != -1)
+		
+			
+				request_floor = (key_signal_1 -'0') * 10 + ( key_signal_2  -'0');
+				lcd_clrscr();
+				lcd_puts("Floor: ");
+				//itoa(request_floor, req_floor_string, 10);
+				lcd_gotoxy(7, 0);
+				lcd_putc(key_signal_1);
+				lcd_putc(key_signal_2);
+				_delay_ms(1000);
+
+				if ((request_floor > 99) || (request_floor < 1))
+				{
+					state = FAULT;
+				}
+				else if (request_floor == current_floor)
+				{
+					state = FAULT;
+				}
+				else if (request_floor > current_floor)
+				{
+					state = GOINGUP;
+				}
+				else if (request_floor < current_floor)
+				{
+					state = GOINGDOWN;
+				}
+					
+				
+			
+
+			break;
+
+		case GOINGUP:
+			if (current_floor < request_floor) 
+			{
+				current_floor++;
+				lcd_clrscr();
+				lcd_puts("Up");
+				lcd_gotoxy(7, 0);
+				//itoa(current_floor, curr_floor_string, 10);
+				lcd_puts("RF:");
+				lcd_putc(request_floor / 10 +'0' );
+				//lcd_gotoxy(0,1);
+				lcd_putc(request_floor % 10 +'0');
+				lcd_gotoxy(0,1);
+				lcd_putc(current_floor / 10 +'0' );
+				//lcd_gotoxy(0,1);
+				lcd_putc(current_floor % 10 +'0');
+				_delay_ms(500);
+			} 
+			else 
+			{
+				state = DOOR;
+			}
+			
+
+			//state = elevator_movement(&current_floor,request_floor);
+			break;
+		case GOINGDOWN:
+		// Movement led on 
+			//lcd_puts("down");
+			//state = elevator_movement(&current_floor,request_floor);
+		if (current_floor > request_floor)
+		{
+			current_floor--;
+			lcd_clrscr();
+			lcd_puts("Down");
+			lcd_gotoxy(7, 0);
+			//itoa(current_floor, curr_floor_string, 10);
+			lcd_puts("RF:");
+			lcd_putc(request_floor / 10 +'0' );
+			//lcd_gotoxy(0,1);
+			lcd_putc(request_floor % 10 +'0');
 			lcd_gotoxy(0,1);
-			lcd_puts(key_str);
+			lcd_putc(current_floor / 10 +'0' );
+			//lcd_gotoxy(0,1);
+			lcd_putc(current_floor % 10 +'0');
+			_delay_ms(500);
+		}
+		else
+		{
+			state = DOOR;
+		}
+		break;
+		
+		case DOOR:
+			lcd_clrscr();
+			lcd_puts("door");
+			_delay_ms(5000);
+			state = IDLE;
+		
+		// door open led to uno (5 sec)
+		// door open 
+		// back to idle state = IDLE
+		
+			
+			break;
+		
+		case FAULT:
+			lcd_puts("fault");
+		break;
+		
+		case EMERGENCY:
+			lcd_puts("emergency");
+			
+			
+			
+			// buzzer on -> plays melody 
+		break;
+		
+		
 		}
 		
-		
-    }
+	
+	
+	}
 }
 
